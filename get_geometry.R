56e82d304bf92a39104a4d6d4ad515fe77efd910
andriy-gazin <andriy.gazin@gmail.com>
2016-09-24 16:00:01 +0300

    basic functions

---
+++ get_geometry.R
@@ -0,0 +1,15 @@
+library(stringr)
+library(jsonlite)
+
+get_geometry <- function(id) {
+  
+  url = paste("http://atu.minregion.gov.ua/api/format/region_template/ato.ato_level_territory_view/atoid/", id, "/wkb_geometry,name_fullua", sep = "")
+  
+  page <- readLines(con = url)
+  
+  geometry <- str_extract(string = page, pattern = '(\\{\\"wkb_geometry\\":.+)') %>% 
+    str_replace("\\}$", "") %>% 
+    fromJSON()
+  
+  return(geometry)
+}
---
+++ get_regions.R
@@ -0,0 +1,29 @@
+library(rvest)
+library(stringr)
+
+get_regions <- function() {
+  
+  startpage <- html_session("http://atu.minregion.gov.ua/ua/ustriy_page/64")
+  
+  name <- startpage %>% 
+  html_nodes(".regions-list a") %>% 
+  html_text()
+  
+  url <- startpage %>% 
+  html_nodes(".regions-list a") %>% 
+  html_attr("href")
+  url <- paste("http://atu.minregion.gov.ua", url, sep = "")
+  
+  id <- url %>% str_extract(pattern = "\\d+?$")
+  
+  regions <- data.frame(id, name, url)
+  
+  return(regions)
+  
+}
+
+
+
+
+  
+  
